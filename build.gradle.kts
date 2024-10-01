41fe460933b1ada17106bbe49ecf53d477982ba5
Neo <c.min.tseng@gmail.com>
2024-10-01 17:35:29 +0800

    modify repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+
+    kotlin("jvm") version libs.versions.kotlin.version.get() apply false
+    alias(libs.plugins.kotlin.android) apply false
+    kotlin("multiplatform") version libs.versions.kotlin.version.get() apply false
+    alias(libs.plugins.kotlin.cocoapods).apply(false)
+
     id("org.jetbrains.compose") version libs.versions.jetbrains.compose.version.get() apply false
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlinx.serialization) apply false
-    alias(libs.plugins.buildKonfigPlugin) apply false
     alias(libs.plugins.compose.compiler).apply(false)
+
+    alias(libs.plugins.ksp.plugin).apply(false)
+
+    alias(libs.plugins.kotlinx.serialization) apply false
+
+    alias(libs.plugins.buildKonfigPlugin) apply false
 }
 
 buildscript {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,20 +2,49 @@ rootProject.name = "ValorantCMP"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
-        google()
         gradlePluginPortal()
+        maven(url = "https://plugins.gradle.org/m2/")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://jogamp.org/deployment/maven")
+    }
+    plugins {
+
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven(url = "https://jogamp.org/deployment/maven")
     }
 }
+
 include(":common")
 include(":android")
 include(":desktop")
